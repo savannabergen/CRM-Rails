1787593ef69eb1e08c6aaceb39bc482ae3dac137
Savanna Bergen <savanna.bergen@gmail.com>
2024-10-17 19:43:08 -0500

    added active admin - crud dashboard complete

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
@@ -12,22 +13,22 @@ ActiveAdmin.register_page "Dashboard" do
 
     # Here is an example of a simple dashboard with columns and panels.
     #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
+     columns do
+       column do
+         panel "Recent Customers" do
+           ul do
+             Customer.all.map do |customer|
+               li link_to(customer.full_name, admin_customer_path(customer))
+             end
+           end
+         end
+       end
 
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
+       column do
+         panel "Info" do
+           para "Welcome to ActiveAdmin."
+         end
+       end
+     end
   end # content
 end
--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -2,7 +2,7 @@ class AdminUser < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   def self.ransackable_attributes(auth_object = nil)
-    ["created_at", "email", "id", "remember_created_at", "reset_password_sent_at", "updated_at"]
+    [ "created_at", "email", "id", "remember_created_at", "reset_password_sent_at", "updated_at" ]
   end
 
   devise :database_authenticatable,

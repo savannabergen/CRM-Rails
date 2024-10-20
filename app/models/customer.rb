580995f56a2f9e3419a3a454003694ccddb2d062
Savanna Bergen <savanna.bergen@gmail.com>
2024-10-20 18:26:19 -0500

    fixed typo in customer.rb - trying magick

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,7 +2,7 @@ class Customer < ApplicationRecord
   has_one_attached :image
 
   def self.ransackable_attributes(auth_object = nil)
-    [ "email", "full_name", "id", "notes", "phone", "customers" ]
+    [ "email", "full_name", "id", "notes", "phone" ]
   end
 
   def self.ransackable_associations(auth_object = nil)

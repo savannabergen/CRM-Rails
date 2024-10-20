782d5bb233f38f9d687e46d61e4e59ffce16e579
Savanna Bergen <savanna.bergen@gmail.com>
2024-10-19 22:17:18 -0500

    rubocop errors - try 2

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,6 @@
 class Customer < ApplicationRecord
   def self.ransackable_attributes(auth_object = nil)
-    ["email", "full_name", "id", "notes", "phone"]
+    [ "email", "full_name", "id", "notes", "phone" ]
   end
 end
+

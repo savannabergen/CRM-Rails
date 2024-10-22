4e4da24d39ede826876f79037b840f6b9e8f88dc
Savanna Bergen <savanna.bergen@gmail.com>
2024-10-22 11:04:03 -0500

    added search functionality

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,9 @@
 class CustomersController < ApplicationController
   def index
-    @customers = Customer.all
+    if params[:search]
+      @customers = Customer.where("lower(full_name) LIKE ?", "%#{params[:search].downcase}%")
+    else
+      @customers = Customer.all
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  get '/customers', to: 'customers#index'
+  get '/customers/search', to: 'customers#index'
 
   # Defines the root path route ("/")
   # root "posts#index"
